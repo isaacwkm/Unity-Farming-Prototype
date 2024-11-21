61dffad6ea6c60e1441e33b5f50c2796a3c550be
isaacwkm@gmail.com <isaacwkm@gmail.com>
2024-11-20 21:06:58 -0800

    changed seed to always progress to initial growth stage 1 after 1 day of being planted

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -7,6 +7,8 @@ public class Plant : MonoBehaviour
     public int daysPerStage = 2;      // Days required to move to the next stage
     private int daysSinceLastGrowth = 0;
 
+    private int ageDays = 0;
+
     private void Start()
     {
         TimeManager timeManager = FindObjectOfType<TimeManager>();
@@ -18,6 +20,14 @@ public class Plant : MonoBehaviour
     // if the plant gets to advance or be stagnant in growth.
     public void AdvanceGrowth()
     {
+        ageDays++;
+
+        if (ageDays == 1){
+            currentStage++;
+            UpdateGrowthStage();
+            return;
+        }
+
         // Condition 1: Water level
         // Condition 2: Sun level
         // If conditions 1 and 2 are met, then below line may run

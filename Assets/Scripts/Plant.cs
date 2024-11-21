ecec9324d0e574b1bea65430f54ada09cf70d402
isaacwkm@gmail.com <isaacwkm@gmail.com>
2024-11-20 21:10:21 -0800

    more comments on plant.cs

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -5,9 +5,8 @@ public class Plant : MonoBehaviour
     public GameObject[] growthStages; // Array of growth stage models
     public int currentStage = 0;      // Current growth stage
     public int daysPerStage = 2;      // Days required to move to the next stage
-    private int daysSinceLastGrowth = 0;
-
-    private int ageDays = 0;
+    private int daysGrowingSinceLastStage = 0;
+    private int ageDays = 0;          // Keeps track of the age of a plant
 
     private void Start()
     {
@@ -31,11 +30,11 @@ public class Plant : MonoBehaviour
         // Condition 1: Water level
         // Condition 2: Sun level
         // If conditions 1 and 2 are met, then below line may run
-        daysSinceLastGrowth++; // f0.f add more conditions for this line to be able to run (check sunlight and water)
+        daysGrowingSinceLastStage++; // f0.f add more conditions for this line to be able to run (check sunlight and water)
 
-        if (daysSinceLastGrowth >= daysPerStage && currentStage < growthStages.Length - 1)
+        if (daysGrowingSinceLastStage >= daysPerStage && currentStage < growthStages.Length - 1)
         {
-            daysSinceLastGrowth = 0;
+            daysGrowingSinceLastStage = 0;
             currentStage++;
             UpdateGrowthStage();
         }
